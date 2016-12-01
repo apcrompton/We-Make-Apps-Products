cd58d88265596247d3653f22968d02b5d8d5099e
Alexander Crompton <acrompton24@bigpond.com>
2016-12-01 13:52:00 +1100

    Initial Commit

---
+++ WeMakeAppsProducts/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  WeMakeAppsProducts
+//
+//  Created by Alexander Crompton on 1/12/2016.
+//  Copyright © 2016 Alex Crompton Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ WeMakeAppsProducts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeMakeAppsProducts
+//
+//  Created by Alexander Crompton on 1/12/2016.
+//  Copyright © 2016 Alex Crompton Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
